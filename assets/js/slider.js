be1f0caea026ff8c58009246fd3f4c40529c29d9
Tiến Trần <tienco201@gmail.com>
2022-07-16 23:38:03 +0700

    new

---
+++ assets/js/slider.js
@@ -0,0 +1,12 @@
+$(".banner__carousel-list").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  infinite: true,
+  autoplaySpeed: 1500,
+  dots: true,
+  prevArrow:
+    "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+  nextArrow:
+    "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+});
